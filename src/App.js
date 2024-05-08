ad320af1ce5355fa7ac684b2104b366673fcfc1c
alqamah <alqama.hasnain@gmail.com>
2024-05-08 14:58:59 +0530

    starting with movie project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Welcome to React course on codingninjas.com</p>    
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  render(){
+      return(
+          <div>
+              <h1>Class Component</h1>
+          </div>
+      )
+    }
 }
 
 export default App;
